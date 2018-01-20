9a0a4ecf0c89e8b33221d7468741cb71760bfc51
vasilij <localducks@gmail.com>
2018-01-20 10:55:42 +0300

    Add upload router to Kotlin server

--- WebFluxServer/src/main/kotlin/com/server/webflux/web/web_config.kt
+++ WebFluxServer/src/main/kotlin/com/server/webflux/web/web_config.kt
@@ -1,34 +1,48 @@
 package com.server.webflux.web
 
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.MediaType
+import org.springframework.core.io.ClassPathResource
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.web.reactive.function.BodyExtractors
+import org.springframework.web.reactive.function.BodyInserters
 import org.springframework.web.reactive.function.server.*
+import org.springframework.web.server.ServerWebExchange
+import org.springframework.web.server.WebFilter
+import org.springframework.web.server.WebFilterChain
+import reactor.core.publisher.Mono
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
 
 @Configuration
-class UserWebConfig {
+class UserWebConfig : WebFilter {
 
-    @Autowired lateinit var userHandler: UserHandler
+    override fun filter(exchange: ServerWebExchange, chain: WebFilterChain): Mono<Void> {
+        val request = exchange.request
+        return if (request.uri.path == "/") {
+            chain.filter(exchange.mutate().request(request.mutate().path("/index.html").build()).build())
+        } else {
+            chain.filter(exchange)
+        }
+    }
 
 
     @Bean
     fun routerFunction(): RouterFunction<ServerResponse> {
-        return RouterFunctions
-                .route(RequestPredicates.GET("/reactive"), HandlerFunction { userHandler.getAllUsers() })
-              //  .andRoute(RequestPredicates.GET("/reactive/{name}"), HandlerFunction { userHandler. }))
+        return RouterFunctions.resources("/**", ClassPathResource("static/"))
+                .andRoute(RequestPredicates.POST("/upload"), HandlerFunction { request ->
+                    request.body(BodyExtractors.toMultipartData()).flatMap { parts ->
+                        val part = parts.toSingleValueMap()
+                        val filePart = part["files"] as FilePart
+                        Files.createFile(Paths.get(filePart.filename()))
+                        filePart.transferTo(File(filePart.filename()))
+                        ServerResponse.ok().body(BodyInserters.fromObject("OK"))
+                    }
+                })
 
     }
 
-//    @Bean
-//    fun routerUpload(): RouterFunction<ServerResponse> {
-//        return RouterFunctions
-//                .route(RequestPredicates.POST("/upload")
-//                        .and(RequestPredicates.contentType(MediaType.MULTIPART_FORM_DATA)), HandlerFunction { uploadHandler.upload() })
-//
-//    }
-
-    @Autowired lateinit var uploadHandler: UploadHandler
 
 }
